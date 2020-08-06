66bdeaa0b00789805f92f0bd982ae17b3ae43af9
RoaMingOne <krum.karapetrov@gmail.com>
2020-08-06 22:39:57 +0300

    User Account Creation and Address book modification

--- src/test/java/ProductReview.java
+++ src/test/java/ProductReview.java
@@ -2,6 +2,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -23,15 +24,35 @@ public class ProductReview {
     @Test
     public void leaveAndApproveReview(){
         driver.get("http://shop.pragmatic.bg/");
+        driver.findElement(By.cssSelector("ul.nav>li:nth-of-type(1)")).click();
         driver.findElement(By.cssSelector("ul.nav>li:nth-of-type(1)>div li:nth-of-type(2)")).click();
         driver.findElement(By.cssSelector("div.caption>h4>a")).click();
         driver.findElement(By.cssSelector("ul.nav-tabs>li:nth-of-type(2)")).click();
-        driver.findElement(By.id("input-name")).sendKeys("KrumK" + RandomStringUtils.randomAlphabetic(1));
-        driver.findElement(By.id("input-review")).sendKeys("Very good product");
-        if (!driver.findElement(By.cssSelector("input[value='5']")).isSelected());{
+        String reviewerName = "KrumK" + RandomStringUtils.randomAlphabetic(1);
+        driver.findElement(By.id("input-name")).sendKeys(reviewerName);
+        driver.findElement(By.id("input-review")).sendKeys("Very good product, worth it");
+        if (!driver.findElement(By.cssSelector("input[value='5']")).isSelected()){
             driver.findElement(By.cssSelector("input[value='5']")).click();
         }
         driver.findElement(By.cssSelector("div.pull-right>button")).click();
-        Assert.assertTrue(driver.findElement(By.cssSelector("div.alert-success")).getText().contains(" Thank you for your review. It has been submitted to the webmaster for approval."));
+//        Assert.assertTrue(driver.findElement(By.cssSelector("div.alert-success")).getText().contains(" Thank you for your review. It has been submitted to the webmaster for approval."));
+        driver.get("http://shop.pragmatic.bg/admin/");
+        driver.findElement(By.id("input-username")).sendKeys("admin22");
+        driver.findElement(By.id("input-password")).sendKeys("parola123!");
+        driver.findElement(By.cssSelector("div.text-right>button")).click();
+        driver.findElement(By.cssSelector("li#menu-catalog")).click();
+        driver.findElement(By.cssSelector("li#menu-catalog li:nth-of-type(9)")).click();
+        Assert.assertTrue(driver.findElement(By.cssSelector("div.table-responsive")).getText().contains(reviewerName));
+        driver.findElement(By.cssSelector("td>a.btn-primary")).click();
+        new Select(driver.findElement(By.id("input-status"))).selectByValue("1");
+        driver.findElement(By.cssSelector("div>button")).click();
+        driver.get("http://shop.pragmatic.bg/");
+        driver.findElement(By.cssSelector("ul.nav>li:nth-of-type(1)")).click();
+        driver.findElement(By.cssSelector("ul.nav>li:nth-of-type(1)>div li:nth-of-type(2)")).click();
+        driver.findElement(By.cssSelector("div.caption>h4>a")).click();
+        driver.findElement(By.cssSelector("ul.nav-tabs>li:nth-of-type(2)")).click();
+        Assert.assertTrue(driver.findElement(By.cssSelector("div#review")).getText().contains("Very good product, worth it"), "review not successful");
+        Assert.assertTrue(driver.findElement(By.cssSelector("div#review")).getText().contains(reviewerName), "review not successful");
+
     }
 }
